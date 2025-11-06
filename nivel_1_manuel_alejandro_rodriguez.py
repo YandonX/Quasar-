8c3234949cc4b51d9b1e3e373db4c701cad2da5a
alejandro <droguelios@gmail.com>
2025-11-06 12:27:28 -0500

    se realizo nivel 1

---
+++ nivel_1_manuel_alejandro_rodriguez.py
@@ -0,0 +1,39 @@
+# Manuel alejandro rodriguez guerrero
+# 1 Solicitar al usuario su nombre de usuario y edad
+print("Hola usuario!")
+usuario = input("Ingrese su nombre de usuario: ")
+edad = int(input("Ingrese su edad: "))
+print(f"tu nombre es {usuario} y tu edad es {edad}")
+
+# 2 suma de dos números
+a = 20
+b = 30
+suma = a + b
+print(f"la suma es:{suma} ")
+
+# 3 Área del triángulo.
+Base = 5
+Altura = 10
+Area = (Base * Altura) / 2
+print(f"el area del triangulo es: {Area}")
+
+# 4 convertir grados Celsius a Fahrenheit
+celsius = 25
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius} grados Celsius son {fahrenheit} grados Fahrenheit")
+
+# 5 typo de datos
+type(usuario)
+type(edad)
+type(suma)
+type(Area)
+print(type(usuario))
+print(type(edad))
+print(type(suma))
+print(type(Area))
+
+#6 calcula cuanto tendra el usuario en 10 años
+edad_futura = edad + 10
+print(f"En 10 años, {usuario} tendrá {edad_futura} años.")
+
+
