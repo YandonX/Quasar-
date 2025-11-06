1598468001963eb7e4b541208c26ff7075303752
alejandro <droguelios@gmail.com>
2025-11-06 18:58:20 -0500

    ejercicio 3

--- nivel_3.py
+++ nivel_3.py
@@ -13,7 +13,31 @@ for i in range(1,11):
     print(f"{multi} x {i} = {multi*i}")'''
 
 # 16 contador con ciclo while
-contador= int(input("Ingrese un numero para iniciar el contador: "))
+'''contador= int(input("Ingrese un numero para iniciar el contador: "))
 while contador>0:
     print(contador)
-    contador-=1
+    contador-=1'''
+# 17 Adivina el número (usar random
+'''import random
+numero_secreto= random.randint(1,10)
+while True:
+    intento = int(input("Adivina el numero: "))
+    if intento == numero_secreto:
+        print("Felicidades! Adivinaste el numero.")
+        break
+    else:
+        print("Intenta de nuevo.")'''
+# 18 Sumar hasta que el usuario escriba 0.
+'''total= 0 
+while True:
+    numero= int (input("ingresa un numero para sumar: "))
+    if numero == 0:
+        break
+    total = total + numero
+print("La suma total es: ", total)'''
+
+        
+
+
+
+
