6b166359d84dc339f23f7e8ae5565059248daca5
alejandro <droguelios@gmail.com>
2025-11-06 14:00:35 -0500

    ejercicio 3

--- nivel_3.py
+++ nivel_3.py
@@ -1,5 +1,19 @@
-# Print numbers from 0 to 10
+# 13 Print numbers from 0 to 10
 '''for i in range(1,11):
     print(i)'''
+# 14 Sumatoria del 1 al n.
+'''n= int(input("Ingrese un numero: "))
+suma=0
+for i in range(1,n+1):
+    suma+=i
+    print("La suma es: ", suma)'''
+# 15 tabla de multiplicar.
+'''multi=int(input("Ingrese un numero para ver su tabla de multiplicar: "))
+for i in range(1,11):
+    print(f"{multi} x {i} = {multi*i}")'''
 
-
+# 16 contador con ciclo while
+contador= int(input("Ingrese un numero para iniciar el contador: "))
+while contador>0:
+    print(contador)
+    contador-=1
