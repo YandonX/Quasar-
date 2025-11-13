dad5414ce1039a8fa42ecd7328aed72c67f2bb72
alejandro <droguelios@gmail.com>
2025-11-13 06:18:10 -0500

    se realizo el punto 5

--- Taller_2.py
+++ Taller_2.py
@@ -1,1 +1,8 @@
-#AQUI VA EL TALLER
+pacientes=[{"name": "Juan", "genero": "M", "edad": 72, "diagnostico": "tos", "historial": "alergias"},
+           {"name": "Juan", "genero": "M", "edad": 50, "diagnostico": "tos", "historial": "alergias"}]
+ancianos=0
+for paciente in pacientes:
+    if paciente["edad"]>60 :
+        print(f"estos son los pacientes ancianos{paciente}")
+        ancianos+=1
+        print(f"cantidad total pacientes {ancianos}")
